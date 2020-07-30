49539ba3a99bcadac08819ec492576ab358a9a29
Andrew Jouffray <andrewjouffray@hotmail.fr>
2020-07-29 18:11:36 -0600

    removed cli visualizer

--- tools/cli-visualizer-master/src/Utils/NcursesUtils.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * NcursesUtils.cpp
- *
- * Created on: Jul 30, 2015
- *     Author: dpayne
- */
-
-#include "Utils/NcursesUtils.h"
--- tools/cli-visualizer-master/src/Utils/Utils.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Utils.cpp
- *
- * Created on: Jul 30, 2015
- *     Author: dpayne
- */
-
-#include "Utils/Utils.h"
